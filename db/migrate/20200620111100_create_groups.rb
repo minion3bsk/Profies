cf436383f53c6ac40b47fcf5b06833bc3abba6f4
長谷川真緒 <chocolate@Mao-MacBook-Pro.local>
2020-06-20 21:31:12 +0900

    RE add column to group

--- db/migrate/20200620111100_create_groups.rb
+++ db/migrate/20200620111100_create_groups.rb
@@ -1,7 +1,8 @@
 class CreateGroups < ActiveRecord::Migration[5.2]
   def change
     create_table :groups do |t|
-
+      t.string :key, unique: true
+      t.text :datail
       t.timestamps
     end
   end
