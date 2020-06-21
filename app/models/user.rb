4f50f31e00eb35d3ddeca9c689133dc9cb8b721a
長谷川真緒 <chocolate@Mao-MacBook-Pro.local>
2020-06-21 20:08:43 +0900

    add attr-accessor and sign_up group_key column

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   belongs_to :group
+  attr_accessor :group_key
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
